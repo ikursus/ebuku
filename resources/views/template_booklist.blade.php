0a516467557b377ce6d5558659569c044ad12695
iKursus <ikursus.com@gmail.com>
2019-01-23 14:32:22 +0800

    Update Blade

--- resources/views/template_booklist.blade.php
+++ resources/views/template_booklist.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts/app')
 
 @section('content')
-<div class="container"> 
+<div class="container">
 
-<h1> <?php echo $title; ?></h1>
+<h1>{{ $title }}</h1>
 <table class="table table-bordered table-stripped">
 
 <thead>
@@ -17,19 +17,22 @@
 
 <tbody>
 
-<?php foreach( $ebooks as $ebook ): ?>
+    @foreach( $ebooks as $ebook )
     <tr>
-        <td><?php echo $ebook['id']; ?></td>
-        <td><?php echo $ebook['title']; ?></td>
-        <td><?php echo $ebook['description']; ?></td>
-        <td><a href="/order/<?php echo $ebook['id']; ?>">ORDER</a></td>
+        <td>{{ $ebook['id'] }}</td>
+        <td>{{ $ebook['title'] }}</td>
+        <td>{{ $ebook['description'] }}</td>
+        <td><a href="/order/{{ $ebook['id'] }}">ORDER</a></td>
     </tr>
-<?php endforeach; ?>
+    @endforeach
 
 </tbody>
 
 </table>
 
 </div>
+@endsection
+
+@section('javascript')
 
 @endsection
