3d12af8620b370758d9840644234c1e206d09843
iKursus <ikursus.com@gmail.com>
2019-01-24 15:26:41 +0800

    Update modal delete

--- resources/views/tempahan/index.blade.php
+++ resources/views/tempahan/index.blade.php
@@ -32,36 +32,36 @@
             <a href="/tempahan/{{ $tempahan->id }}" class="btn btn-sm btn-primary">KEMASKINI</a>
 
             <!-- Button trigger modal -->
-<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-delete-{{ $tempahan->id }}">
-  DELETE
-</button>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-delete-{{ $tempahan->id }}">
+            DELETE
+            </button>
 
-<form method="POST" action="{{ route('tempahan.destroy', $tempahan->id) }}">
+            <form method="POST" action="{{ route('tempahan.destroy', $tempahan->id) }}">
                 @csrf
                 @method('DELETE')
 
-                <!-- Modal -->
-<div class="modal fade" id="modal-delete-{{ $tempahan->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Pengesahan Delete</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Adakah anda bersetuju untuk menghapuskan data ini?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-danger">DELETE</button>
-      </div>
-    </div>
-  </div>
-</div>
-                
-            </form>
+                            <!-- Modal -->
+            <div class="modal fade" id="modal-delete-{{ $tempahan->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Pengesahan Delete</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Adakah anda bersetuju untuk menghapuskan data ini?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger">DELETE</button>
+                </div>
+                </div>
+            </div>
+            </div>
+            
+        </form>
 
 
             
